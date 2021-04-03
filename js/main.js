c1fd3890e4b73e4a4b9e05f0a28956b56871bbf5
jesuisunjerome <38988287+jesuisunjerome@users.noreply.github.com>
2021-04-03 01:55:46 -0600

    Modal

--- js/main.js
+++ js/main.js
@@ -1,1 +1,72 @@
+(function () {
+    'use strict'
 
+    OpenModalLogIn();
+    FormValidation();
+})()
+
+function FormValidation() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+}
+
+function OpenModalLogIn() {
+    $('#btnHeaderCreerCompte').click(function () {
+        $('#pnlForgotPassword').hide();
+        $('#pnlLogin').hide();
+        $('#pnlSignUp').show();
+
+        var myModal = new bootstrap.Modal(document.getElementById('modalLog'))
+        myModal.show();
+
+        return false;
+    })
+
+    $('#btnHeaderSeConnecter').click(function () {
+        $('#pnlForgotPassword').hide();
+        $('#pnlSignUp').hide();
+        $('#pnlLogin').show();
+
+        var myModal = new bootstrap.Modal(document.getElementById('modalLog'))
+        myModal.show();
+
+        return false;
+    })
+
+    $("#switchToSeConnecter").click(function () {
+        $('#pnlForgotPassword').hide();
+        $('#pnlSignUp').hide();
+        $('#pnlLogin').show();
+
+        return false;
+    })
+
+    $("#switchToCreerCompte").click(function () {
+        $('#pnlForgotPassword').hide();
+        $('#pnlLogin').hide();
+        $('#pnlSignUp').show();
+
+        return false;
+    })
+
+    $("#switchToForgotPassword").click(function () {
+        $('#pnlLogin').hide();
+        $('#pnlSignUp').hide();
+        $('#pnlForgotPassword').show();
+
+        return false;
+    })
+}
